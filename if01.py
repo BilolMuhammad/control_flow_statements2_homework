cb784f1f509d0bbb9316a1e91ade9c52b7b6b31d
PRESTIGE <mamatqulov@gmail.com>
2023-01-03 11:09:23 +0500

    01

--- if01.py
+++ if01.py
@@ -1,4 +1,4 @@
-def main(a,b,c):
+def main(a, b, c):
     """
     Find the largest of the numbers.
     Args:
@@ -8,4 +8,15 @@ def main(a,b,c):
     Returns:
         int: return answer.
     """
+    if a > b:
+        if a > c:
+            return a
+        return c
+    if b > a:
+        if b > c:
+            return b
+        return c
     return
+
+
+print(main(6, 7, 9))
