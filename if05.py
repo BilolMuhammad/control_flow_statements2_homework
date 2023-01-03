0f44b626d4f7213f18f46068f85c5967c665aa02
PRESTIGE <mamatqulov@gmail.com>
2023-01-03 16:11:33 +0500

    05

--- if05.py
+++ if05.py
@@ -6,4 +6,26 @@ def main(n):
     Returns:
         int: return answer.
     """
-    return
+    n1 = n % 10
+    n //= 10
+    n2 = n % 10
+    n //= 10
+    n3 = n % 10
+    n //= 10
+    n4 = n % 10
+    n //= 10
+    n5 = n % 10
+    max = n1
+    if n2 > max:
+        max = n2
+    elif n3 > max:
+        max = n3
+    elif n4 > max:
+        max = n4
+    elif n5 > max:
+        max = n5
+    return max
+
+
+print(main(43819))
+print(main(72198))
